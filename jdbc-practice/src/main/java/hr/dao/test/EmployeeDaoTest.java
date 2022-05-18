46b4d5a0560ae90d306b2d98256cd4ff0a08cf3f
Dragon <alemfla3181@naver.com>
2022-05-18 16:54:42 +0900

    jdbc

--- jdbc-practice/src/main/java/hr/dao/test/EmployeeDaoTest.java
+++ jdbc-practice/src/main/java/hr/dao/test/EmployeeDaoTest.java
@@ -8,12 +8,22 @@ import hr.vo.EmployeeVo;
 public class EmployeeDaoTest {
 
 	public static void main(String[] args) {
-		testFindByFirstNameOrLastName("ken");
+		// testFindByFirstNameOrLastName("ken");
+		testFindBySalary(20000, 40000);
 	}
 
 	public static void testFindByFirstNameOrLastName(String name) {
 		List<EmployeeVo> list = new EmployeeDao().findByFirstNameOrLastName(name);
-		for (EmployeeVo vo : list)
+		for (EmployeeVo vo : list) {
 			System.out.println(vo);
+		}
 	}
+
+	public static void testFindBySalary(int min, int max) {
+		List<EmployeeVo> list = new EmployeeDao().findBySalary(min, max);
+		for (EmployeeVo vo : list) {
+			System.out.println(vo);
+		}
+	}
+
 }
