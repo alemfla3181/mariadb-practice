48b736a35b88f524574b5bb742813d08fc456e2d
Dragon <alemfla3181@naver.com>
2022-05-18 12:08:44 +0900

    jdbc #기본 final

---
+++ jdbc-practice/sql/employee-department.sql
@@ -0,0 +1,15 @@
+-- DepartmentDao:findAll
+select no, name
+from department
+order by no desc;
+
+-- EmployeeDao:findAll
+select no, name, department_no
+from employee
+order by no desc;
+
+-- DepartmentDao:update
+update department set name = '솔루션개발' where no = 3;
+
+-- EmployeeDao:delete()
+delete from employee;
